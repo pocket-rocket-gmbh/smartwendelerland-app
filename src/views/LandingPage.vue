99c7ba3a39cca5a3a5b9f44dd92676567df327e6
Jörn Koch <jk@pocket-rocket.io>
2022-07-04 18:11:59 +0200

    made landing page completely dynamic based on icons per row and total width of the screen

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,29 +1,25 @@
 <template>
   <base-layout>
     <ion-grid>
-      <ion-row>
-        <ion-col><ion-button router-link="/participation"><ion-icon :ios="accessibilityOutline" :md="accessibilitySharp"/></ion-button></ion-col>
-        <ion-col><ion-button router-link="/login"><ion-icon :ios="logInOutline" :md="logInSharp"/></ion-button></ion-col>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-        <ion-col><ion-button><ion-icon :ios="constructOutline" :md="constructSharp"/></ion-button></ion-col>
-      </ion-row>
+      <ion-row v-for="(row, rowIndex) in rowCount" :key="rowIndex">
+        <ion-col v-for="(column, columnIndex) in columnCount" :key="columnIndex">
+          <template v-if="items[rowIndex * columnCount + columnIndex] !== undefined">
+            <ion-button :router-link="items[rowIndex * columnCount + columnIndex].link">
+              <ion-icon :ios="items[rowIndex * columnCount + columnIndex].iosIcon" :md="items[rowIndex * columnCount + columnIndex].mdIcon"/>
+            </ion-button>
+            <div class="label">{{ items[rowIndex * columnCount + columnIndex].name }}</div>
+          </template>
+        </ion-col>        
+      </ion-row>      
     </ion-grid>
   </base-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
-import { IonGrid, IonRow, IonCol, IonButton, IonIcon, onIonViewDidEnter } from '@ionic/vue'
+import { defineComponent, ref, computed, onMounted } from 'vue'
+import { IonGrid, IonRow, IonCol, IonButton, IonIcon, } from '@ionic/vue'
 import { accessibilityOutline, accessibilitySharp, logInOutline, logInSharp, constructOutline, constructSharp } from 'ionicons/icons'
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation'
 import BaseLayout from '@/components/general/BaseLayout.vue'
 
 export default defineComponent({
@@ -31,33 +27,89 @@ export default defineComponent({
   components: { BaseLayout, IonGrid, IonRow, IonCol, IonButton, IonIcon },
   setup() {
 
-    const windowwidth = ref(window.innerWidth)
+    const columnCountPortrait = 3
+    const columnCountLandscape = 6
 
-    onIonViewDidEnter(() => {
+    const items = [
+      {
+        name: 'Bürgerbeteiligung',
+        link: '/participation',
+        iosIcon: accessibilityOutline,
+        mdIcon: accessibilitySharp
+      },
+      {
+        name: 'Login Test',
+        link: '/login',
+        iosIcon: logInOutline,
+        mdIcon: logInSharp
+      },
+      {
+        name: 'Placeholder',
+        link: '/',
+        iosIcon: constructOutline,
+        mdIcon: constructSharp
+      },
+      {
+        name: 'Placeholder',
+        link: '/',
+        iosIcon: constructOutline,
+        mdIcon: constructSharp
+      }
+    ]
+
+    const windowwidth = ref(window.innerWidth)
+    const isLandscape = ref(false)
+
+    onMounted(() => {
+      updateOrientation()
+
+      ScreenOrientation.onChange().subscribe(() => {
+        updateOrientation()
+      })
+
       window.onresize = () => {
         windowwidth.value = window.innerWidth
       }
     })
 
+    function updateOrientation() {
+      isLandscape.value = ScreenOrientation.type == ScreenOrientation.ORIENTATIONS.LANDSCAPE || ScreenOrientation.type == ScreenOrientation.ORIENTATIONS.LANDSCAPE_PRIMARY
+    }
+
+    const rowCount = computed(() => {
+      return Math.ceil(items.length / columnCount.value)
+    })
+
+    const columnCount = computed(() => {
+      return isLandscape.value ? columnCountLandscape : columnCountPortrait
+    })
+
     const padding = computed(() => {
-      return windowwidth.value * 0.025 + 'px'
+      const result = (windowwidth.value - (buttonSizeNumeric.value * columnCount.value)) / paddingCount.value - 2 + 'px' // 2 pixel per padding removed to account for window border.
+      return result
+    })
+
+    // Each button has left and right padding and the grid itself has left and right padding.
+    const paddingCount = computed(() => {
+      return columnCount.value * 2 + 2
     })
 
     const iconsize = computed(() => {
-      return windowwidth.value * 0.125 + 'px'
+      return buttonSizeNumeric.value / 2 + 'px'
     })
 
     const buttonsize = computed(() => {
-      return windowwidth.value * 0.25 + 'px'
+      return buttonSizeNumeric.value + 'px'
+    })
+
+    const buttonSizeNumeric = computed(() => {
+      return windowwidth.value / (columnCount.value + columnCount.value / 3)
     })
 
     return {
-      accessibilityOutline,
-      accessibilitySharp,
-      logInOutline,
-      logInSharp,
-      constructOutline,
-      constructSharp,
+      items,
+      rowCount,
+      columnCount,
       padding,
       buttonsize,
       iconsize
@@ -82,5 +134,14 @@ ion-button {
 
 ion-icon {
   font-size: v-bind('iconsize');
+}
+
+div.label {
+  padding-top: 10px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: v-bind('buttonsize');
 }
 </style>
