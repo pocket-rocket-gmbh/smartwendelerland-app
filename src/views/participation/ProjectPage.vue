2a491535abe7657d670fe3e04ceddcb69f13f656
Jörn Koch <jk@pocket-rocket.io>
2022-07-06 10:35:34 +0200

    implemented loading of comments

--- src/views/participation/ProjectPage.vue
+++ src/views/participation/ProjectPage.vue
@@ -7,21 +7,54 @@
 
       <ion-grid v-if="project">
         <ion-row>
-          <img :src="project.image_url"/>
+          <ion-col>
+            <img :src="project.image_url"/>
+          </ion-col>
         </ion-row>
         <ion-row>
-          <ion-label class="headline">{{ project.name }}</ion-label>
+          <ion-col>
+            <ion-label class="headline">{{ project.name }}</ion-label>
+          </ion-col>
         </ion-row>
         <ion-row>
-          <ion-label>{{ useDatetime().getTimeRangeString(project) }}</ion-label>
+          <ion-col>
+            <ion-label>{{ useDatetime().getTimeRangeString(project) }}</ion-label>
+          </ion-col>
+        </ion-row>        
+        <ion-row>
+          <ion-col>
+            <ion-label>Baukosten: {{ useCurrency().getCurrencyFromNumber(project.costs) }}</ion-label>
+          </ion-col>
         </ion-row>
         <ion-row>
-          <ion-label>Baukosten: {{ useCurrency().getCurrencyFromNumber(project.costs) }}</ion-label>
+          <ion-col>
+            <ion-label>{{ project.description }}</ion-label>
+          </ion-col>
         </ion-row>
         <ion-row>
-          <ion-label>{{ project.description }}</ion-label>
+          <ion-col>
+            <ion-label>Kommentare</ion-label>
+          </ion-col>
         </ion-row>
+        <ion-row v-if="!useUserStore().user">
+          <ion-col>
+            <ion-label>Bitte einloggen, um die Kommentare zu sehen</ion-label>
+          </ion-col>
+        </ion-row>
+        <template v-else>
+          <ion-row v-if="comments.length === 0">
+            <ion-col>
+              <ion-label>Keine Kommentare gefunden</ion-label>
+            </ion-col>
+          </ion-row>
+          <ion-row v-else v-for="comment in comments" :key="comment.id">
+            <ion-col>
+              <ion-label>Kommentar: {{ comment }}</ion-label>
+            </ion-col>
+          </ion-row>
+        </template>
       </ion-grid>
+
       <ion-loading
         :is-open="loadingInProgress"
         message="Projekt wird geladen..."
@@ -33,51 +66,71 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { useRoute } from 'vue-router'
-import { IonContent, IonRefresher, IonRefresherContent, IonGrid, IonRow, IonLabel, IonLoading, onIonViewWillEnter, RefresherCustomEvent } from '@ionic/vue'
+import { IonContent, IonRefresher, IonRefresherContent, IonGrid, IonRow, IonCol, IonLabel, IonLoading, onIonViewWillEnter, RefresherCustomEvent } from '@ionic/vue'
 import BaseLayout from '@/components/general/BaseLayout.vue'
 import { usePublicApi } from '@/composables/api/public'
 import { useCollectionApi } from '@/composables/api/collectionApi'
 import { useDatetime } from '@/composables/ui/datetime'
 import { useCurrency } from '@/composables/ui/currency'
+import { useUserStore } from '@/stores/user'
+import { usePrivateApi } from '@/composables/api/private'
 
 export default defineComponent({
   name: 'ParticipationProjectListPage',
-  components: { BaseLayout, IonContent, IonRefresher, IonRefresherContent, IonGrid, IonRow, IonLabel, IonLoading },
+  components: { BaseLayout, IonContent, IonRefresher, IonRefresherContent, IonGrid, IonRow, IonCol, IonLabel, IonLoading },
   setup() {
 
     const route = useRoute()
-    const publicApi = usePublicApi()
-    const api = useCollectionApi()
-    api.setBaseApi(publicApi)
-    api.setEndpoint('projects')
 
-    const project = api.item
+    const publicApi = usePublicApi()
+    const projectsApi = useCollectionApi()
+    projectsApi.setBaseApi(publicApi)
+    projectsApi.setEndpoint('projects')
+
+    const privateApi = usePrivateApi()
+    const commentsApi = useCollectionApi()
+    commentsApi.setBaseApi(privateApi)
+
+    const project = projectsApi.item
+    const comments = commentsApi.items
 
     const loadingInProgress = ref(false)
 
     onIonViewWillEnter(() => {
-      loadProject()
+      reloadData()
     })
 
     const doRefresh = (event: RefresherCustomEvent) => {
-      loadProject()
+      reloadData()
       event.target.complete() // We have a separate loading indicator so we can complete the refresh indicator.
     }
 
-    const loadProject = () => {
+    const reloadData = async () => {
       loadingInProgress.value = true
-      api.getItem(route.params.id?.toString()).then(() => {
-        loadingInProgress.value = false
-        console.log(project.value)
-      })      
+
+      await Promise.all([
+        projectsApi.getItem(route.params.id?.toString()),
+        loadComments()
+      ])
+
+      loadingInProgress.value = false      
+    }
+
+    const loadComments = async () => {
+      if (useUserStore().user) {
+        commentsApi.setEndpoint('comments/project/' + route.params.id?.toString())
+        commentsApi.retrieveCollection()
+      }
     }
 
     return {
       loadingInProgress,
       doRefresh,
       project,
+      comments,
       useDatetime,
-      useCurrency
+      useCurrency,
+      useUserStore
     }
   }
 })
