300160ee381777676b0fb23e562d5f62bedff5eb
Jörn Koch <jk@pocket-rocket.io>
2022-07-08 16:18:21 +0200

    added delete functionality for comments (no page refresh yet)

--- src/components/participation/CommentPanel.vue
+++ src/components/participation/CommentPanel.vue
@@ -6,7 +6,7 @@
           {{ comment.user.name }}
         </ion-col>
         <ion-col style="text-align: right">
-          ...
+          <ion-label @click="showContext($event)">...</ion-label>
         </ion-col>
       </ion-row>
       <ion-row>
@@ -20,23 +20,69 @@
         </ion-col>
       </ion-row>
     </ion-grid>
+    
+    <ion-popover
+      :is-open="popoverOpen"
+      :translucent="true"
+      :event="commentContextEvent"
+      @didDismiss="closePopover"
+    >
+      <ion-button @click="deleteComment()">Löschen</ion-button>
+    </ion-popover>
   </ion-item>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { IonItem, IonGrid, IonRow, IonCol } from '@ionic/vue'
+import { defineComponent, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { IonItem, IonGrid, IonRow, IonCol, IonLabel, IonPopover, IonButton } from '@ionic/vue'
 import { useDatetime } from '@/composables/ui/datetime'
+import { usePrivateApi } from '@/composables/api/private'
+import { useCollectionApi } from '@/composables/api/collectionApi'
 
 export default defineComponent({
   name: 'ParticipationCommentPanel',
   props: {
     comment: Object
   },
-  components: { IonItem, IonGrid, IonRow, IonCol },
-  setup() {
+  components: { IonItem, IonGrid, IonRow, IonCol, IonLabel, IonPopover, IonButton },
+  setup(props) {
+
+    const route = useRoute()
+
+    const privateApi = usePrivateApi()
+    const commentsApi = useCollectionApi()
+    commentsApi.setBaseApi(privateApi)
+    commentsApi.setEndpoint('comments')
+
+    const popoverOpen = ref(false)
+    const commentContextEvent = ref(null)
+
+    const showContext = (event: PointerEvent) => {
+      commentContextEvent.value = event
+      popoverOpen.value = true
+    }
+
+    const closePopover = () => {
+      commentContextEvent.value = null
+      popoverOpen.value = false
+    }
+
+    const deleteComment = () => {
+      commentsApi.setEndpoint('comments')
+      commentsApi.deleteItem(props.comment.id)
+      commentsApi.setEndpoint('comments/project/' + route.params.id?.toString())
+      commentsApi.retrieveCollection()
+      closePopover()
+    }
+
     return {
-      useDatetime
+      useDatetime,
+      showContext,
+      popoverOpen,
+      commentContextEvent,
+      closePopover,
+      deleteComment
     }
   }
 })
