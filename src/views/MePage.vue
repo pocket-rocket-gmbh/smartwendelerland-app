dee34dc7b9fef6886815918dee02687382544db9
Jörn Koch <jk@pocket-rocket.io>
2022-07-11 09:58:21 +0200

    added basic me page implementation

--- src/views/MePage.vue
+++ src/views/MePage.vue
@@ -1,18 +1,90 @@
 <template>
   <base-layout>
-    <WipLayout />
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-label class="headline">Ihre Daten</ion-label>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+          <ion-label>Vorname:</ion-label>
+        </ion-col>
+        <ion-col>
+          <ion-label>{{ userStore.user?.firstname }}</ion-label>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+          <ion-label>Nachname:</ion-label>
+        </ion-col>
+        <ion-col>
+          <ion-label>{{ userStore.user?.lastname }}</ion-label>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+          <ion-label>E-Mail:</ion-label>
+        </ion-col>
+        <ion-col>
+          <ion-label>{{ userStore.user?.email }}</ion-label>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+          <ion-label>Landkreis:</ion-label>
+        </ion-col>
+        <ion-col>
+          <ion-label>{{ userStore.user?.organization.name }}</ion-label>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+          <ion-button @click="logout">Abmelden</ion-button>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
   </base-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { IonGrid, IonRow, IonCol, IonLabel, IonButton, onIonViewWillEnter } from '@ionic/vue'
 import BaseLayout from '@/components/general/BaseLayout.vue'
-import WipLayout from '@/components/WipLayout.vue'
+import { useUserStore } from '@/stores/user'
 
 export default defineComponent({
   name: 'MePage',
-  components: { WipLayout, BaseLayout }
+  components: { BaseLayout, IonGrid, IonRow, IonCol, IonLabel, IonButton },
+  setup() {
 
-  // TODO send to login screen if not logged in
+    const router = useRouter()
+    const userStore = useUserStore()
+
+    onIonViewWillEnter(() => {
+      if (userStore.user === null) {
+        router.push('/login')
+      }
+    }) 
+
+    const logout = () => {
+      // TODO call API to perform logout
+      localStorage.removeItem('auth._token.jwt')
+      userStore.user = null
+      router.push('/')
+    }
+
+    return {
+      userStore,
+      logout
+    }
+  }
 })
 </script>
+
+<style scoped>
+.headline {
+  font-size: 1.5em;
+}
+</style>
