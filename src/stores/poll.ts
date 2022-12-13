dcf40c06d88bb50c669b0cc9ee2cc1059f9c7547
donmb1 <martinbr83@gmail.com>
2022-12-13 17:39:15 +0100

    staging endpoint

--- src/stores/poll.ts
+++ src/stores/poll.ts
@@ -14,14 +14,18 @@ export const usePollStore = defineStore({
       api.setBaseApi(usePrivateApi())
       api.setEndpoint(`polls?is_active=true&ignore_projects=true`)
       const result = await api.retrieveCollection({ page: 1, per_page: 1000, sort_by: 'created_at', sort_order: 'DESC', searchQuery: null, concat: false, filters: null })
-      this.publicPoll = result.data.resources[0]
+      if (result.data) {
+        this.publicPoll = result.data.resources[0]
+      }
     },
     async setProjectPoll(projectId:string) {
       const api = useCollectionApi()
       api.setBaseApi(usePrivateApi())
       api.setEndpoint(`polls?is_active=true&project_id=${projectId}`)
       const result = await api.retrieveCollection({ page: 1, per_page: 1000, sort_by: 'created_at', sort_order: 'DESC', searchQuery: null, concat: false, filters: null })
-      this.projectPoll = result.data.resources[0]
+      if (result.data) {
+        this.projectPoll = result.data.resources[0]
+      }
     },
   }
 })
