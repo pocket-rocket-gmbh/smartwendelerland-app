097a2ae9c23562eb6bafb7fe9ae8bd85238adc8f
donmb1 <martinbr83@gmail.com>
2023-07-17 15:38:16 +0200

    skeleton

---
+++ src/views/health/CategoriesPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <BaseLayout>
+    <ion-content class="ion-padding">
+      Hallo
+    </ion-content>
+</BaseLayout>
+</template>
+
+<script setup lang="ts">
+import { IonContent } from '@ionic/vue';
+import BaseLayout from '@/components/general/BaseLayout.vue'
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/landing/HealthPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <BaseLayout>
+    Platzhalter Tutorial
+    <ion-nav-link routerLink="/health/categories">
+      Weiter
+    </ion-nav-link>
+  </BaseLayout>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+import { IonNavLink } from '@ionic/vue'
+import BaseLayout from '@/components/general/BaseLayout.vue'
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation'
+import { onIonViewDidEnter } from '@ionic/vue'
+import { useRouter } from 'vue-router'
+
+const windowwidth = ref(window.innerWidth)
+const isLandscape = ref(false)
+const router = useRouter()
+
+const updateOrientation = () => {
+  isLandscape.value = ScreenOrientation.type == ScreenOrientation.ORIENTATIONS.LANDSCAPE || ScreenOrientation.type == ScreenOrientation.ORIENTATIONS.LANDSCAPE_PRIMARY
+}
+
+onIonViewDidEnter(() => {
+  const projektplattformTutorialSkipped = localStorage.getItem('gesundheitsplattform_tutorial_skipped')
+  if (projektplattformTutorialSkipped) {
+    if (projektplattformTutorialSkipped === 'true') {
+      router.push({ path: '/health'})
+    }
+  }
+})
+
+onMounted(() => {
+  updateOrientation()
+
+  ScreenOrientation.onChange().subscribe(() => {
+    updateOrientation()
+  })
+
+  window.onresize = () => {
+    windowwidth.value = window.innerWidth
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
