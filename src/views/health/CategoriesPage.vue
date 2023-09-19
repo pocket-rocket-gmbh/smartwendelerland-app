c9cc32c9b41513374c0275dc84f6d222fabd93a3
NormanF <nf@pocket-rocket.io>
2023-09-19 15:32:34 +0200

    clear searchterm when view gets reentered

--- src/views/health/CategoriesPage.vue
+++ src/views/health/CategoriesPage.vue
@@ -11,10 +11,15 @@
 </template>
 
 <script setup lang="ts">
-import { IonContent } from '@ionic/vue';
+import { IonContent, onIonViewWillEnter } from '@ionic/vue';
 import BaseLayout from '@/components/general/BaseLayout.vue'
 import SearchPanel from '@/components/health/categories/SearchPanel.vue'
 import MainCategories from '../../components/health/categories/MainCategories.vue';
+import { useFilterStore } from '@/stores/health/searchFilter';
+
+onIonViewWillEnter(()=> {
+  useFilterStore().currentSearchTerm = "";
+})
 
 </script>
 
