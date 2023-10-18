058beb62027d24a4c4c8ba4c303b2f56004e3715
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-10-18 18:19:57 +0200

    - design changes for scope select

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,22 +1,22 @@
 <template>
   <BaseLayout>
-    <ion-grid>
+    <ion-grid class="main-page">
       <ion-row class="ion-justify-content-center">
-        <ion-col size-sm="12" size-md="6" size-lg="3">
+        <ion-col size-sm="12" size-md="12" size-lg="12">
           <ion-nav-link routerLink="/landing/participation">
             <div class="pile has-background-project" align="center">
-              <img src="@/assets/images/panel_project.svg" alt="project" />
-              <div class="headline">Projekt- plattform</div>
+              <img class="icon-project" src="@/assets/images/panel_project.svg" alt="project" />
+              <div class="headline">Projekt-plattform</div>
             </div>
           </ion-nav-link>
         </ion-col>
       </ion-row>
       <ion-row class="ion-justify-content-center">
-        <ion-col size-sm="12" size-md="6" size-lg="3">
+        <ion-col size-sm="12" size-md="12" size-lg="12">
           <ion-nav-link routerLink="/health/categories">
             <div class="pile has-background-health" align="center">
-              <img src="@/assets/images/panel_health.svg" alt="health" />
-              <div class="headline">Gesundheits- plattform</div>
+              <img class="icon-health" src="@/assets/images/panel_health.svg" alt="health" />
+              <div class="headline">Gesundheits-plattform</div>
             </div>
           </ion-nav-link>
         </ion-col>
@@ -56,17 +56,16 @@ onMounted(() => {
 
 <style lang="sass" scoped>
 .pile
-  width: 80%
+  width: 90%
   padding: 20px
   border-radius: 20px
   margin: 0 auto
   position: relative
-
+  height: 35vh
   display: flex
   flex-direction: column
   justify-content: center
   align-items: center
-
   font-size: 35px
   font-style: normal
   line-height: 35px
@@ -76,6 +75,21 @@ onMounted(() => {
   .headline
     text-transform: uppercase
     color: white
+    font-size: 2rem
     text-align: center
     margin-top: 20px
+
+.main-page
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-content: center
+  height: 100%
+  gap: 5%
+
+.icon-health
+  width: 80%
+
+.icon-project
+  width: 60%
 </style>
