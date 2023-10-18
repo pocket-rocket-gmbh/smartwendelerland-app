747da0bb38b785e1f7d528ddf5b67d23dd7578e5
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-10-18 18:15:17 +0200

    - removed login action on menu bar on start page

--- src/components/general/BaseLayout.vue
+++ src/components/general/BaseLayout.vue
@@ -2,46 +2,57 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <img @click="$router.push({path: `/`})" src="@/assets/images/logo.png" class="logo" slot="start"/>
-        <ion-icon v-if="!useUser().loggedIn() && showLogin" @click="router.push('/login')" router-link="/login" :ios="logInOutline" :md="logInSharp" slot="end"></ion-icon>
-        <div v-else-if="showLogin" @click="router.push('/me')" router-link="/me" slot="end">
-          <UserProfile
-            :user="useUser().currentUser()"
-          />
+        <img @click="$router.push({ path: '/' })" src="@/assets/images/logo.png" class="logo" slot="start" />
+        <ion-icon
+          v-if="!useUser().loggedIn() && projekRoute"
+          @click="router.push('/login')"
+          router-link="/login"
+          slot="end"
+        ></ion-icon>
+        <div
+          v-else-if="projekRoute"
+          @click="router.push('/me')"
+          router-link="/me"
+          slot="end"
+        >
+          <UserProfile :user="useUser().currentUser()" />
         </div>
       </ion-toolbar>
     </ion-header>
-
     <ion-content :fullscreen="true">
       <slot />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { defineProps } from 'vue'
+<script lang="ts" setup>
+import { computed } from 'vue'
 import { IonPage, IonHeader, IonToolbar, IonIcon, IonContent } from '@ionic/vue'
-import { logInOutline, logInSharp, personCircleOutline, personCircleSharp } from 'ionicons/icons'
 import { useRouter } from 'vue-router'
 import UserProfile from '@/components/UserProfile.vue'
 import { useUser } from '@/composables/user/user'
 
 const router = useRouter()
+const route = router.currentRoute
 
-defineProps({
-  showLogin: {
-    type: Boolean,
-    default: true
+const projekRoute = computed(() => {
+  if (route.value.path.includes('participation')) {
+    return true
   }
-})
-
+  return false;
+});
 </script>
 
 <style lang="sass" scoped>
 .logo
-  width: 80px
+  width: 100px
   margin-left: 10px
 ion-icon
   font-size: 32px
   padding-right: 20px
+
+ion-toolbar 
+  --border-width: 4px 0
+  --border-style: double
+  --min-height: 80px
 </style>
