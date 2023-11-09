f24d77dc740a2e92d11a796acfdd214928d62bb3
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-11-09 14:31:01 +0100

    - reworked community filter to work in tabs as buttons

--- src/components/health/CommunityFilter.vue
+++ src/components/health/CommunityFilter.vue
@@ -1,25 +1,39 @@
 <template>
-  <ion-select
-    :toggle-icon="caretDownSharp"
-    interface="popover"
-    placeholder="Gemeinde wählen"
-    v-model="selectedFilter"
-  >
-    <ion-select-option
-      v-for="community in communities"
-      :key="community.id"
-      :value="community"
-    >
-      {{ community.name }}
-    </ion-select-option>
-  </ion-select>
+  <ion-content>
+    <div class="filter-options communities ion-padding">
+      <label
+        :for="community.id"
+        class="option"
+        v-for="community in communities"
+        :key="community.id"
+        :value="community"
+      >
+        <div
+          :model-value="selectedFilter"
+          @click.prevent="selectCommunityFilterValue(community)"
+          hide-details
+          density="compact"
+          class="options-select communities"
+          :class="{
+            'is-selected': filterStore.currentZip === community.zip,
+          }"
+        >
+          {{ community.name }}
+        </div>
+      </label>
+    </div>
+  </ion-content>
 </template>
 
 <script setup lang="ts">
 import { useCollectionApi } from "@/composables/api/collectionApi";
 import { usePublicApi } from "@/composables/api/public";
-import { onMounted, ref, watch, defineEmits, defineExpose } from "vue";
-import { IonSelect, IonSelectOption } from "@ionic/vue";
+import { onMounted, ref, defineEmits, defineExpose } from "vue";
+import {
+
+  useFilterStore,
+} from "@/stores/health/searchFilter";
+const filterStore = useFilterStore();
 const emit = defineEmits(["selectCommunityFilter"]);
 
 const selectedFilter = ref(null);
@@ -39,9 +53,12 @@ onMounted(() => {
   getCommunities();
 });
 
-watch(selectedFilter, () => {
+const selectCommunityFilterValue = (selectedFilter: any) => {
+  console.log("selectedFilter", selectedFilter)
+  selectedFilter.value = selectedFilter;
   emit("selectCommunityFilter", selectedFilter.value);
-});
+};
+
 
 defineExpose({ resetFilter });
 </script>
