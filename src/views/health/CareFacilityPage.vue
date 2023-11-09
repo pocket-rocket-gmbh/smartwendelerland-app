21b99aea1d04918338e36825c0413ab835b5cf94
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-11-09 14:33:17 +0100

    - init redesign facilities details page

--- src/views/health/CareFacilityPage.vue
+++ src/views/health/CareFacilityPage.vue
@@ -1,10 +1,18 @@
 <template>
-  <BackButtonLayout :force-back="`/health/search?kind=${facility?.kind}`" :show-login="false">
-
+  <BackButtonLayout
+    :force-back="`/health/search?kind=${facility?.kind}`"
+    :show-login="false"
+    :title="facility?.name"
+  >
     <div class="ion-padding" v-if="facility">
-      <swiper :slides-per-view="1" :space-between="20" :modules="modules" :pagination="{ el: '.pagination' }"
-        v-if="facility.image_url" class="header-image swiper"
-        style="--swiper-pagination-color: #8AB61D; --swiper-pagination-top: 8px;"
+      <swiper
+        :slides-per-view="1"
+        :space-between="20"
+        :modules="modules"
+        :pagination="{ el: '.pagination' }"
+        v-if="facility.image_url"
+        class="header-image swiper"
+        style="--swiper-pagination-color: #8ab61d; --swiper-pagination-top: 8px"
       >
         <swiper-slide>
           <img :src="imageCache.cacheableImageUrl(facility.image_url)" class="showroom" />
@@ -17,57 +25,104 @@
       </swiper>
 
       <div class="headline ion-margin-top">
-        {{ facility.name }} <img src="@/assets/images/check-decagram-outline.svg" class="insurance-logo" v-if="facility.billable_through_health_insurance_approved" />
+        {{ facility.name }}
+        <img
+          src="@/assets/images/check-decagram-outline.svg"
+          class="insurance-logo"
+          v-if="facility.billable_through_health_insurance_approved"
+        />
       </div>
       <div class="has-text-health tags ion-margin-bottom" v-if="facility.tags.length > 0">
         <span v-for="tag in facility.tags" :key="tag.id">{{ tag.name }}</span>
       </div>
 
-      <div v-html="facility.description" class="general-font-size"/>
-      <div v-if="facility.name_responsible_person" class="ion-margin-bottom"><i>Inhaltlich verantwortlich: {{ facility.name_responsible_person }}</i></div>
+      <div v-html="facility.description" class="general-font-size" />
+      <div v-if="facility.name_responsible_person" class="ion-margin-bottom">
+        <i>Inhaltlich verantwortlich: {{ facility.name_responsible_person }}</i>
+      </div>
       <div v-if="facility.kind === 'news'" class="news-grid">
-        <div><span><img src="@/assets/images/watch.svg"></span> {{ useDatetime().parseDatetime(facility.created_at) }}
+        <div>
+          <span><img src="@/assets/images/watch.svg" /></span>
+          {{ useDatetime().parseDatetime(facility.created_at) }}
         </div>
 
-        <div v-if="facility.user"><span><img src="@/assets/images/user.svg"></span> {{ facility.user.name }}</div>
+        <div v-if="facility.user">
+          <span><img src="@/assets/images/user.svg" /></span> {{ facility.user.name }}
+        </div>
       </div>
       <div v-else class="more-infos ion-padding">
         <div>
-          <div class="has-text-health is-uppercase ion-margin-bottom">Kontakt und Infos</div>
-          <div v-if="facility.phone" class="info-grid">
-            <div><img src="@/assets/images/facilities/icon_phone.svg"></div>
-            <div class="general-font-size"><a :href="`tel:${facility.phone}`">{{ facility.phone }}</a></div>
+          <div class="has-text-health headline is-uppercase ion-margin-bottom">
+            Kontakt und Infos
           </div>
-          <div v-if="facility.email" class="info-grid">
-            <div><img src="@/assets/images/facilities/icon_mail.svg"></div>
-            <div class="general-font-size"><a :href="`mailto:${facility.email}`">{{ facility.email }}</a></div>
-          </div>
-          <div v-if="facility.street" class="info-grid">
-            <div><img src="@/assets/images/facilities/icon_address.svg"></div>
-            <div class="general-font-size">
-              <div>{{ facility.street }}</div>
-              <div>{{ facility.zip }} {{ facility.town }}</div>
+          <ion-grid>
+            <ion-row>
+              <ion-col size-sm="12" size-md="8">
+                <div v-if="facility.phone" class="info-grid">
+                  <div><img src="@/assets/images/facilities/icon_phone.svg" /></div>
+                  <div class="general-font-size">
+                    <a :href="`tel:${facility.phone}`">{{ facility.phone }}</a>
+                  </div>
+                </div>
+                <div v-if="facility.email" class="info-grid">
+                  <div><img src="@/assets/images/facilities/icon_mail.svg" /></div>
+                  <div class="general-font-size">
+                    <a :href="`mailto:${facility.email}`">{{ facility.email }}</a>
+                  </div>
+                </div>
+                <div v-if="facility.street" class="info-grid">
+                  <div><img src="@/assets/images/facilities/icon_address.svg" /></div>
+                  <div class="general-font-size">
+                    <div>{{ facility.street }}</div>
+                    <div>{{ facility.zip }} {{ facility.town }}</div>
+                  </div>
+                </div>
+                <div v-if="facility.name_instructor">
+                  <div
+                    class="has-text-health is-uppercase ion-margin-bottom ion-margin-top"
+                  >
+                    Veranstalter
+                  </div>
+                  <div>{{ facility.name_instructor }}</div>
+                </div>
+              </ion-col>
+              <ion-col>
+                <div
+                  v-if="facility.website"
+                  class="button-rounded is-uppercase ion-margin-top"
+                  @click="handleLinkClick(getWebsiteLink(facility.website))"
+                >
+                  Weitere Informationen
+                  <ion-icon :icon="openOutline" />
+                </div>
+                <div class="ion-margin-top">
+                  <a
+                    v-if="facility.email"
+                    class="button-rounded is-uppercase"
+                    :href="`mailto:${facility.email}`"
+                  >
+                    Kontakt aufnehmen
+                  </a>
+                </div>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+
+          <div
+            v-if="
+              facility.opening_hours.length > 0 &&
+              openingHoursFilled(facility.opening_hours)
+            "
+            class="opening-hours"
+          >
+            <div
+              class="has-text-health is-uppercase ion-margin-top headline ion-margin-bottom"
+            >
+              Öffnungszeiten
             </div>
-          </div>
-          <div v-if="facility.name_instructor">
-            <div class="has-text-health is-uppercase ion-margin-bottom ion-margin-top">Veranstalter</div>
-            <div>{{ facility.name_instructor }}</div>
-          </div>
-          <div v-if="facility.website" class="button-rounded is-uppercase ion-margin-top" @click="handleLinkClick(getWebsiteLink(facility.website))">
-            Weitere Informationen
-            <ion-icon :icon="openOutline" />
-          </div>
-          <div class="ion-margin-top">
-            <a v-if="facility.email" class="button-rounded is-uppercase" :href="`mailto:${facility.email}`">
-              Kontakt aufnehmen
-            </a>
-          </div>
-          <div v-if="facility.opening_hours.length > 0 && openingHoursFilled(facility.opening_hours)" class="opening-hours">
-            <div class="has-text-health is-uppercase ion-margin-top ion-margin-bottom">Öffnungszeiten
-            </div>
-            <div class="general-font-size">
+            <div class="general-font-size ion-no-padding">
               <ion-row v-for="opening in facility.opening_hours" :key="opening.day">
-                <ion-col>
+                <ion-col size="4" size-md="2">
                   <ion-label>{{ opening.day }}</ion-label>
                 </ion-col>
                 <ion-col v-if="opening.hours.length">
@@ -79,10 +134,19 @@
         </div>
       </div>
 
-      <div class="more-infos ion-margin-top ion-padding" v-if="facility.event_dates.length > 0">
-        <div class="has-text-health font-size-medium is-uppercase ion-margin-bottom">Termine</div>
+      <div
+        class="more-infos ion-margin-top ion-padding"
+        v-if="facility.event_dates.length > 0"
+      >
+        <div class="has-text-health font-size-medium is-uppercase ion-margin-bottom">
+          Termine
+        </div>
         <div class="ion-margin-bottom">
-          {{ facility.event_dates.length }} Termin<span v-if="facility.event_dates.length > 1">e</span> gefunden
+          {{ facility.event_dates.length }} Termin<span
+            v-if="facility.event_dates.length > 1"
+            >e</span
+          >
+          gefunden
         </div>
         <table class="table">
           <thead>
@@ -91,81 +155,92 @@
           </thead>
           <tbody>
             <tr v-for="(date, index) in mapDates(facility.event_dates)" :key="index">
-              <td>{{ date.getDate() }}.{{ formattedDateString(date.getMonth() + 1) }}.{{ date.getFullYear() }}</td>
-              <td>{{ date.getHours() }}:{{ formattedDateString(date.getMinutes()) }} Uhr</td>
+              <td>
+                {{ date.getDate() }}.{{ formattedDateString(date.getMonth() + 1) }}.{{
+                  date.getFullYear()
+                }}
+              </td>
+              <td>
+                {{ date.getHours() }}:{{ formattedDateString(date.getMinutes()) }} Uhr
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
 
-      <div class="more-infos ion-margin-top ion-padding" v-for="document in facility.sanitized_documents"
-        :key="document.signed_id" @click="handleLinkClick(document.url)">
-        <div class="has-text-health font-size-medium is-uppercase ion-margin-bottom">Dokumente</div>
+      <div
+        class="more-infos ion-margin-top ion-padding"
+        v-for="document in facility.sanitized_documents"
+        :key="document.signed_id"
+        @click="handleLinkClick(document.url)"
+      >
+        <div class="has-text-health font-size-medium is-uppercase ion-margin-bottom">
+          Dokumente
+        </div>
 
         <div>
-          <img src="@/assets/images/download.svg">
+          <img src="@/assets/images/download.svg" />
           <span>{{ document.name }}.pdf</span>
         </div>
       </div>
     </div>
 
-
     <ion-loading :is-open="loading" message="Wird geladen..." />
   </BackButtonLayout>
 </template>
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Pagination } from 'swiper'
-import 'swiper/css'
-import "swiper/css/pagination"
-import { computed, ref } from 'vue';
-import BackButtonLayout from '@/components/general/BackButtonLayout.vue'
-import { usePublicApi } from '@/composables/api/public';
-import { useCollectionApi } from '@/composables/api/collectionApi';
-import { onIonViewDidEnter, IonIcon } from '@ionic/vue';
-import { useRoute } from 'vue-router';
-import { Browser } from '@capacitor/browser'
-import { useImageCache } from '@/composables/ui/imageCache'
-import { useDatetime } from '@/composables/ui/datetime';
-import { openOutline } from 'ionicons/icons'
-import { checkDecagramOutline } from '@/assets/images/check-decagram-outline.svg'
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { computed, ref } from "vue";
+import BackButtonLayout from "@/components/general/BackButtonLayout.vue";
+import { usePublicApi } from "@/composables/api/public";
+import { useCollectionApi } from "@/composables/api/collectionApi";
+import { onIonViewDidEnter, IonIcon } from "@ionic/vue";
+import { useRoute } from "vue-router";
+import { Browser } from "@capacitor/browser";
+import { useImageCache } from "@/composables/ui/imageCache";
+import { useDatetime } from "@/composables/ui/datetime";
+import { openOutline } from "ionicons/icons";
+import { checkDecagramOutline } from "@/assets/images/check-decagram-outline.svg";
 
-const route = useRoute()
-const loading = ref(false)
-const imageCache = useImageCache()
+const route = useRoute();
+const loading = ref(false);
+const imageCache = useImageCache();
 
 const facilityId = computed(() => {
-  return route.params.id as string
-})
+  return route.params.id as string;
+});
 
-const showApi = useCollectionApi()
-const facility = showApi.item
+const showApi = useCollectionApi();
+const facility = showApi.item;
 
 const getItem = async () => {
-  showApi.setBaseApi(usePublicApi('health'))
-  showApi.setEndpoint(`care_facilities`)
-  loading.value = true
-  await showApi.getItem(facilityId.value)
-  loading.value = false
-}
+  showApi.setBaseApi(usePublicApi("health"));
+  showApi.setEndpoint(`care_facilities`);
+  loading.value = true;
+  await showApi.getItem(facilityId.value);
+  loading.value = false;
+};
 
 const handleLinkClick = async (url: string) => {
-  await Browser.open({ url: url })
-}
+  await Browser.open({ url: url });
+};
 
 onIonViewDidEnter(() => {
-  getItem()
-})
+  getItem();
+});
 
 const getWebsiteLink = (url: string) => {
-  if (url.startsWith('http') || url.startsWith('https')) {
-    return url
+  if (url.startsWith("http") || url.startsWith("https")) {
+    return url;
   } else {
-    return `https://${url}`
+    return `https://${url}`;
   }
-}
+};
 
-const parseDate = (dateString:string) => {
+const parseDate = (dateString: string) => {
   const parts = dateString.split(/[\s.:-]/);
   const day = parseInt(parts[0], 10);
   const month = parseInt(parts[1] - 1, 10);
@@ -174,34 +249,33 @@ const parseDate = (dateString:string) => {
   const minute = parseInt(parts[4], 10);
 
   const date = new Date(year, month, day, hour, minute);
-  
+
   // Convert to Berlin timezone
   date.toLocaleString("de-DE");
-  
+
   return date;
-}
+};
 
-const mapDates = (dateStrings:string[]) => {
-  let dates = dateStrings.map(parseDate)
+const mapDates = (dateStrings: string[]) => {
+  let dates = dateStrings.map(parseDate);
 
-  return dates.sort((a:any, b:any) => a - b)
-}
+  return dates.sort((a: any, b: any) => a - b);
+};
 
-const formattedDateString = (rawNumber:number) => {
-  return rawNumber < 10 ? `0${rawNumber}` : rawNumber
-}
+const formattedDateString = (rawNumber: number) => {
+  return rawNumber < 10 ? `0${rawNumber}` : rawNumber;
+};
 
-const openingHoursFilled = (openingHours:any[]) => {
+const openingHoursFilled = (openingHours: any[]) => {
   for (const item of openingHours) {
     if (item.hours.trim() !== "") {
       return true;
     }
   }
   return false;
-}
+};
 
-const modules = [Pagination]
-
+const modules = [Pagination];
 </script>
 <style lang="sass">
 .header-image img
