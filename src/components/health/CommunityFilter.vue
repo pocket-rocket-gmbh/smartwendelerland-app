bfb24311a996b6b31175f1f10754b0eb209de30b
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-11-24 17:26:29 +0100

    - new communities select layout

--- src/components/health/CommunityFilter.vue
+++ src/components/health/CommunityFilter.vue
@@ -1,6 +1,6 @@
 <template>
   <ion-content>
-    <div class="filter-options communities ion-padding">
+    <div class="filter-options communities">
       <label
         :for="community.id"
         class="option"
@@ -22,7 +22,6 @@
         </div>
       </label>
     </div>
-
   </ion-content>
   <ion-loading :is-open="loadingFilters" message="Gemeinde werden geladen..." />
 </template>
@@ -32,7 +31,17 @@ import { useCollectionApi } from "@/composables/api/collectionApi";
 import { usePublicApi } from "@/composables/api/public";
 import { onMounted, ref, defineEmits, defineExpose } from "vue";
 import { useFilterStore } from "@/stores/health/searchFilter";
-import { IonButton, IonContent, IonFooter, IonHeader, IonTitle, IonToast, IonToolbar, IonLoading } from '@ionic/vue';
+import {
+  IonButton,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonTitle,
+  IonToast,
+  IonToolbar,
+  IonLoading,
+  toastController,
+} from "@ionic/vue";
 const filterStore = useFilterStore();
 const emit = defineEmits(["selectCommunityFilter"]);
 
@@ -52,6 +61,17 @@ const resetFilter = () => {
   selectedFilter.value = null;
 };
 
+const presentToast = async (selectedFilter: any) => {
+  const toast = await toastController.create({
+    message: `Gemeinde ${selectedFilter.name} ausgewählt`,
+    duration: 3000,
+    cssClass: "custom-toast",
+   
+  });
+
+  await toast.present();
+};
+
 onMounted(() => {
   getCommunities();
 });
@@ -60,14 +80,23 @@ const selectCommunityFilterValue = (selectedFilter: any) => {
   console.log("selectedFilter", selectedFilter);
   selectedFilter.value = selectedFilter;
   emit("selectCommunityFilter", selectedFilter.value);
+  presentToast(selectedFilter);
 };
 
 defineExpose({ resetFilter });
 </script>
 
-<style lang="sass" scoped>
-ion-select
-  background: white
-  border-radius: 7px
-  border: 1px solid black
+<style>
+ion-toast.custom-toast {
+  --background: #8ab61d;
+  --box-shadow: 3px 3px 10px 0 rgba(0, 0, 0, 0.2);
+  --color: #4b4a50;
+}
+
+ion-toast.custom-toast::part(message) {
+  font-style: italic;
+  color: white;
+  font-size: 1.2rem;
+}
+
 </style>
