ebf120686f795ba522a4775db603852aef56cf60
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-11-24 17:27:34 +0100

    - new search panel layout

--- src/components/health/categories/SearchPanel.vue
+++ src/components/health/categories/SearchPanel.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="search-panel">
-    <div class="health-panel ion-padding">
-      <div class="has-text-white general-font-size-title ion-padding text-wrap">
+    <div class="health-panel">
+      <div class="gap-1"></div>
+      <div class="has-text-white general-font-size-title text-wrap">
         Herzlich Willkommen auf deiner Gesundheitsplattform <br />
        im Landkreis Sankt Wendel
       </div>
-      <div class="has-text-white general-font-size-subtitle ion-padding text-wrap">
+      <div class="gap-1"></div>
+      <div class="has-text-white general-font-size-subtitle text-wrap">
         Finde Informationen zu Gesundheitsthemen, Präventionsmaßnahmen, sowie
         umfangreiche Pflegeangebote.
       </div>
+      <div class="gap-1"></div>
     </div>
   </div>
 </template>
