f3f297f2bd1c8f6c4c2c5920ecfee57133a9a749
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-12-19 15:38:41 +0100

    - health branch has now a tutorial when entering for the first time

--- src/components/health/IntroTutorial.vue
+++ src/components/health/IntroTutorial.vue
@@ -2,7 +2,7 @@
   <ion-grid>
     <ion-row class="ion-justify-content-center">
       <ion-col size-sm="12" size-md="6" size-lg="3">
-        <div class="ion-padding">
+        <div class="general-font-size">
           <swiper
             :slides-per-view="1"
             :space-between="20"
@@ -12,45 +12,44 @@
             <swiper-slide>
               <div class="slide">
                 <div class="circle">
-                  <img src="@/assets/images/tutorial-sheep.png" />
+                  <img src="@/assets/images/tutorial/icon_virus.svg" />
                 </div>
-                <div class="headline">
-                  “Willkommen in der Smart Wendeler App”
+                <div class="headline" lang="de">
+                  Willkommen in der gesundesWND-App
                 </div>
-                <p>In diesem Intro erfährst du, was du in der App machen kannst.</p>
+                <div class="hypernate ion-no-padding tutorial-description" lang="de">
+                  Gesundheitsanbieter, News, Veranstaltungen- wir informieren dich rund um
+                  dieThemen Gesundheit, Prävention und Pflege im Smart Wendeler Land.
+                </div>
               </div>
             </swiper-slide>
             <swiper-slide>
               <div class="slide">
                 <div class="circle">
-                  <img src="@/assets/images/tutorial-rating.png" />
+                  <img src="@/assets/images/main-categories/icon_app_facilities.svg" />
                 </div>
-                <div class="headline">
-                  Bürgerumfragen
+                <div class="headline" lang="de">
+                  Erlebe das Gesundheitsangebot im Smart Wendeler Land
                 </div>
-                <p>Nimm an Umfragen teil und sag uns deine Meinung.</p>
+                <div class="hypernate ion-no-padding tutorial-description" lang="de">
+                  Lass dich inspirieren oder finde mit unserer Such- und Filterfunktion
+                  gezielt den passenden Gesundheitsanbieter für dich.
+                </div>
               </div>
             </swiper-slide>
             <swiper-slide>
               <div class="slide">
                 <div class="circle">
-                  <img src="@/assets/images/tutorial-poll.png" />
+                  <img src="@/assets/images/main-categories/icon_app_events.svg" />
                 </div>
-                <div class="headline">
-                  Projekte bewerten
+                <div class="headline" lang="de">
+                  Entdecke vielfältige Veranstaltungs- und Kursangebote
                 </div>
-                <p>Gib deine Stimme zu einem Projekt ab und gestalte mit.</p>
-              </div>
-            </swiper-slide>
-            <swiper-slide>
-              <div class="slide">
-                <div class="circle">
-                  <img src="@/assets/images/tutorial-feedback.png" />
+                <div class="hypernate ion-no-padding tutorial-description" lang="de">
+                  Wir unterstützen dich bei der Suche nach dem passenden Präventionskurs
+                  aus den Bereichen Entspannung, Ernährung, Fitness oder Sucht und
+                  informiere dich über bevorstehende Events.
                 </div>
-                <div class="headline">
-                  Community
-                </div>
-                <p>Kommuniziere mit dem Projektteam und Projektbeteiligten, oder teile das Projekt mit Freunden.</p>
               </div>
             </swiper-slide>
             <div class="pagination" />
@@ -63,74 +62,80 @@
       </ion-col>
     </ion-row>
   </ion-grid>
-  <div class="bottom-skip-tutorial" @click="skipIntro">
+  <div class="bottom-skip-tutorial general-font-size" @click="skipIntro">
     Intro überspringen
   </div>
 </template>
 
 <script lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Pagination } from 'swiper'
-import 'swiper/css'
-import "swiper/css/pagination"
-import { defineComponent } from 'vue'
-import { IonGrid, IonRow, IonCol } from '@ionic/vue'
-import { useRouter } from 'vue-router'
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { defineComponent } from "vue";
+import { IonGrid, IonRow, IonCol } from "@ionic/vue";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
-  name: 'LandingPage',
+  name: "LandingPage",
   components: { IonGrid, IonRow, IonCol, Swiper, SwiperSlide },
   setup() {
-
-    const router = useRouter()
+    const router = useRouter();
     const skipIntro = () => {
-      localStorage.setItem('projektplattform_tutorial_skipped', 'true')
-      router.push({ path: '/participation/projects'})
-    }
-
+      localStorage.setItem("gesundheitsplattform_tutorial_skipped", "true");
+      router.push({ path: "/health/categories" });
+    };
     return {
       skipIntro,
-      modules: [Pagination]
-    }
-  }
-})
+      modules: [Pagination],
+    };
+  },
+});
 </script>
 
 <style lang="sass" scoped>
 .slide
-  background: linear-gradient(270deg, #017DC2 0.29%, #015281 100%)
+  background: linear-gradient(74deg, #91A80D 0%, #BAC323 38.19%, #9EA100 95.31%)
   border-radius: 20px
-  height: 70vh
+  height: 65vh
   text-align: center
   color: #FFFFFF
-  padding: 40px 20px 20px 20px
+  padding: 20px 20px 20px 20px
   position: relative
+  display: flex
+  flex-direction: column
+  justify-content: center
+  gap: 30px
+  @media (max-width: 365px)
+    gap: 10px
+    height: 60vh
   .circle
     margin: 0 auto
-    background: #3CB5E7
-    width: 150px
-    height: 150px
-    border: 11px solid #004E79
+    background: white
+    width: 110px
+    height: 110px
+    border: 5px solid #007344
     border-radius: 50%
     display: grid
     place-items: center
+    @media (max-width: 365px)
+      width: 70px
+      height: 70px
     img
-      width: 90%
-      margin-left: 10px
-      margin-top: -10px
+      width: 70%
+      margin-top: -5px
+      @media (max-width: 365px)
+        margin-top: 0px
   .headline
     font-style: normal
     font-weight: 500
-    font-size: 25px
-    line-height: 22px
-    letter-spacing: 0.09em
-    text-transform: uppercase
+    font-size: 27px
     color: #FFFFFF
-    margin: 40px 0
+    margin: 10px 0
 .pagination
   text-align: center
 .forward
-  background: linear-gradient(19deg, #FEE02D 37.19%, #D3A604 77.42%)
+  background: var(--ion-color-health)
   border-radius: 20px
   text-transform: uppercase
   padding: 10px 30px
@@ -143,7 +148,7 @@ export default defineComponent({
   right: 0
 
 .bottom-skip-tutorial
-  background: linear-gradient(270deg, #017DC2 0.29%, #015281 100%)
+  background: linear-gradient(74deg, #91A80D 0%, #BAC323 38.19%, #9EA100 95.31%)
   text-transform: uppercase
   color: white
   height: 72px
@@ -152,4 +157,8 @@ export default defineComponent({
   place-items: center
   position: absolute
   bottom: 0
+
+.swiper-pagination-bullet .swiper-pagination-bullet-active
+  background-color: black!important
+  color: red!important
 </style>
