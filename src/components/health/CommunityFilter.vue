b78867117fb6d9f5dffbefb70d7c603c78bab40a
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-12-19 15:31:50 +0100

    - filter without facilities are now shown as disabled - modal padding bottom

--- src/components/health/CommunityFilter.vue
+++ src/components/health/CommunityFilter.vue
@@ -1,6 +1,6 @@
 <template>
-  <ion-content class="ion-padding-top">
-    <div class="filter-options communities">
+  <ion-content class="ion-padding-top ">
+    <div class="filter-options communities content-wrap">
       <label
         :for="community.id"
         class="option"
@@ -10,20 +10,33 @@
       >
         <div
           :model-value="selectedFilter"
+          v-if="community?.care_facilities_active_count > '0'"
           @click.prevent="selectCommunityFilterValue(community)"
           hide-details
           density="compact"
-          class="options-select communities"
+          class="options-select communities general-font-size"
           :class="{
             'is-selected': filterStore.currentZip === community.zip,
           }"
         >
           {{ community.name }}
         </div>
+        <div
+          v-else
+          class="options-select is-disabled hypernate general-font-size"
+          lang="de"
+        >
+          {{ community.name }}
+        </div>
       </label>
     </div>
   </ion-content>
-  <ion-loading :is-open="loadingFilters" message="Gemeinde werden geladen..." />
+  <ion-loading
+    class="is-dark-grey"
+    mode="md"
+    :is-open="loadingFilters"
+    message="Gemeinde werden geladen..."
+  />
 </template>
 
 <script setup lang="ts">
@@ -72,5 +85,6 @@ defineExpose({ resetFilter });
 </script>
 
 <style lang="sass" scoped>
-
+.content-wrap
+  padding-bottom: 150px
 </style>
