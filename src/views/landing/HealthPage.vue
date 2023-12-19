2c4b59d3c9d9edcd1733c1fe65c2758c604122a4
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-12-19 15:45:28 +0100

    - activated health tutorial

--- src/views/landing/HealthPage.vue
+++ src/views/landing/HealthPage.vue
@@ -1,53 +1,35 @@
 <template>
   <BaseLayout>
-    Platzhalter Tutorial
-    <ion-nav-link routerLink="/health/categories">
-      Weiter
-    </ion-nav-link>
+    <IntroTutorial />
   </BaseLayout>
 </template>
 
-<script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+<script lang="ts">
+import { defineComponent } from "vue";
 
-import { IonNavLink } from '@ionic/vue'
-import BaseLayout from '@/components/general/BaseLayout.vue'
-import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation'
-import { onIonViewDidEnter } from '@ionic/vue'
-import { useRouter } from 'vue-router'
-import { useFilterStore } from '@/stores/health/searchFilter'
+import IntroTutorial from "@/components/health/IntroTutorial.vue";
+import BaseLayout from "@/components/general/BaseLayout.vue";
+import { onIonViewDidEnter } from "@ionic/vue";
+import { useRouter } from "vue-router";
 
-const windowwidth = ref(window.innerWidth)
-const isLandscape = ref(false)
-const router = useRouter()
-const filterStore = useFilterStore()
+export default defineComponent({
+  name: "LandingPage",
+  components: { BaseLayout, IntroTutorial },
+  setup() {
+    const router = useRouter();
 
-const updateOrientation = () => {
-  isLandscape.value = ScreenOrientation.type == ScreenOrientation.ORIENTATIONS.LANDSCAPE || ScreenOrientation.type == ScreenOrientation.ORIENTATIONS.LANDSCAPE_PRIMARY
-}
-
-onIonViewDidEnter(() => {
-  const projektplattformTutorialSkipped = localStorage.getItem('gesundheitsplattform_tutorial_skipped')
-  if (projektplattformTutorialSkipped) {
-    if (projektplattformTutorialSkipped === 'true') {
-      router.push({ path: '/health'})
-    }
-  }
-})
-
-onMounted(() => {
-  updateOrientation()
-
-  ScreenOrientation.onChange().subscribe(() => {
-    updateOrientation()
-  })
-
-  window.onresize = () => {
-    windowwidth.value = window.innerWidth
-  }
-})
+    onIonViewDidEnter(() => {
+      const gesundheitsplattformTutorialSkipped = localStorage.getItem(
+        "gesundheitsplattform_tutorial_skipped"
+      );
+      if (gesundheitsplattformTutorialSkipped) {
+        if (gesundheitsplattformTutorialSkipped === "true") {
+          router.push({ path: "/health/categories" });
+        }
+      }
+    });
+  },
+});
 </script>
 
-<style lang="sass" scoped>
-
-</style>
+<style lang="sass" scoped></style>
