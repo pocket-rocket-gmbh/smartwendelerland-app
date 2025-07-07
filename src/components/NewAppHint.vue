34f0a9cb165411d6f3e6df022cbb9b4e38c69383
Sascha Metz <sascha.metz@pocket-rocket.io>
2025-07-07 10:33:01 +0200

    improve device detection for apphint

--- src/components/NewAppHint.vue
+++ src/components/NewAppHint.vue
@@ -18,14 +18,14 @@
         <footer class="ion-padding">
           <IonButton
             href="https://apps.apple.com/de/app/meinwnd/id6741873749"
-            v-if="isPlatform('ios')"
+            v-if="isIOSDevice"
             expand="full"
           >
             meinWND im AppStore
           </IonButton>
           <IonButton
             href="https://play.google.com/store/apps/details?id=smawela.pocketrocket.io"
-            v-if="isPlatform('android')"
+            v-if="isAndroidDevice"
             expand="full"
           >
             meinWND im PlayStore
@@ -48,8 +48,32 @@ import {
   isPlatform,
 } from "@ionic/vue";
 import { useSessionStorage } from "@vueuse/core";
+import { computed } from "vue";
 
 const isOpen = useSessionStorage("meinWndHintOpen", true);
+
+const isIOSDevice = computed(() => {
+  if (isPlatform("ios")) return true;
+
+  const userAgent = navigator.userAgent || navigator.vendor;
+  if (/iPad|iPhone|iPod/.test(userAgent)) return true;
+
+  if (navigator.platform) {
+    return ["iPhone", "iPad", "iPod"].includes(navigator.platform);
+  }
+
+  return (
+    /iPad|iPhone|iPod/.test(navigator.userAgent) ||
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+  );
+});
+
+const isAndroidDevice = computed(() => {
+  if (isPlatform("android")) return true;
+
+  const userAgent = navigator.userAgent || navigator.vendor;
+  return /android/i.test(userAgent);
+});
 </script>
 
 <style lang="css">
@@ -58,7 +82,7 @@ footer {
 
   left: 0;
   right: 0;
-  bottom: 0;
+  bottom: 2vh;
   width: 100%;
 }
 </style>
